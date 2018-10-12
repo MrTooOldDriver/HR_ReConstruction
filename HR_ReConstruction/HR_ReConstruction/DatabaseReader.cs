40481b9f8e6142dbc32c5f6a40ca7576a3810273
hantao zhong <moon991015@gmail.com>
2018-10-13 07:46:17 +0800

    Initial commit

---
+++ HR_ReConstruction/HR_ReConstruction/DatabaseReader.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HR_ReConstruction.Properties;
+
+
+namespace HR_ReConstruction
+{
+
+    class DatabaseReader
+    {
+        public long imageStreamLocation { get; set; }
+        public long lableStreamLocation { get; set; }
+        public int pixNumber { get; set; }
+
+        public byte[] readBytes()
+        {
+            string imagePath = @"train-images.idx3-ubyte";
+            string lablePath = @"train-labels.idx1-ubyte";
+            FileStream imageStream = new FileStream(imagePath, FileMode.Open);
+            FileStream lableStream = new FileStream(lablePath, FileMode.Open);
+            BinaryReader imageReader = new BinaryReader(imageStream);
+            BinaryReader lableReader = new BinaryReader(lableStream);
+
+            byte[] pixInfo = new byte[0];
+
+            if (imageStreamLocation == 0)
+            {
+                int magNumber = imageReader.ReadInt32();
+                int imageNumber = imageReader.ReadInt32();
+                int rowNumber = imageReader.ReadInt32();
+                int colNumber = imageReader.ReadInt32();
+
+                magNumber = convertByte(magNumber);
+                imageNumber = convertByte(imageNumber);
+                rowNumber = convertByte(rowNumber);
+                colNumber = convertByte(colNumber);
+
+                pixNumber = rowNumber * colNumber;
+
+                pixInfo = new byte[pixNumber + 1];
+                for (int i = 0; i < pixNumber; i++)
+                {
+                    pixInfo[i] = imageReader.ReadByte();
+                }
+
+                imageStreamLocation = imageStream.Position;
+            }
+            else
+            {
+                imageStream.Seek(imageStreamLocation, 0);
+                pixInfo = new byte[pixNumber + 1];
+                for (int i = 0; i < pixNumber; i++)
+                {
+                    pixInfo[i] = imageReader.ReadByte();
+                }
+
+                imageStreamLocation = imageStream.Position;
+                //MessageBox.Show(imageStream.Position.ToString());
+            }
+
+            imageStream.Close();
+
+            if (lableStreamLocation == 0)
+            {
+                int magNumber = lableReader.ReadInt32();
+                int itemNumber = lableReader.ReadInt32();
+
+                magNumber = convertByte(magNumber);
+                itemNumber = convertByte(itemNumber);
+
+                pixInfo[pixNumber] = lableReader.ReadByte();
+                lableStreamLocation = lableStream.Position;
+            }
+            else
+            {
+                lableStream.Seek(lableStreamLocation, 0);
+                pixInfo[pixNumber] = lableReader.ReadByte();
+                lableStreamLocation = lableStream.Position;
+            }
+
+            lableStream.Close();
+
+            //MessageBox.Show(lableStreamLocation + "and" + imageStreamLocation);
+
+            return pixInfo;
+        }
+
+        public int convertByte(int inputInt)
+        {
+            byte[] inputByte = BitConverter.GetBytes(inputInt);
+            Array.Reverse(inputByte);
+            return BitConverter.ToInt32(inputByte, 0);
+        }
+
+    }
+}
---
+++ HR_ReConstruction/HR_ReConstruction/ReadDatabase.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HR_ReConstruction
+{
+    /// <summary>
+    /// ReadDatabase.xaml 的交互逻辑
+    /// </summary>
+    public partial class ReadDatabase : Window
+    {
+        public ReadDatabase()
+        {
+            InitializeComponent();
+        }
+
+        private DatabaseReader databaseReader = new DatabaseReader(); // Object Database reader
+
+        private readonly DatabaseReader newDatabase = new DatabaseReader();
+
+        public double[] CoodCheck(int NumberOfPix, int mag)
+        {
+            double x = 50;
+            double y = 50;
+            x = x + NumberOfPix % 28 * mag;
+            y = y + NumberOfPix / 28 * mag;
+            var re = new double[2];
+            re[0] = x;
+            re[1] = y;
+            return re;
+        }
+
+        private void Drawit(byte[] vs)
+        {
+            // Draw out the image from the MINST database
+            this.Can.Children.Clear();
+            var mag = 10; // Scale Factor of image
+            this.Can.Margin = new Thickness(0, 0, 0, 0); // Create new canvas object
+            for (var count = 0; count < vs.Length - 1; count++)
+            {
+                // display each pixs
+                var r = new Rectangle();
+                var pixinforReverse = Convert.ToByte(255 - vs[count]); // RGB convert
+                r.Fill = new SolidColorBrush(Color.FromRgb(pixinforReverse, pixinforReverse, pixinforReverse));
+                r.Width = mag;
+                r.Height = mag;
+                var db = this.CoodCheck(count, mag); // coordinate Check and calculation of picture
+                var x = db[0];
+                var y = db[1];       
+                r.SetValue(Canvas.LeftProperty, x);
+                r.SetValue(Canvas.TopProperty, y);
+                this.Can.Children.Add(r); // draw picture
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            var vs = this.newDatabase.readBytes(); // Get data from database
+            this.Drawit(vs); // draw it out
+            var vs_Position = vs.Length;
+            this.itemLable.Content = vs[vs_Position - 1];
+        }
+    }
+}
